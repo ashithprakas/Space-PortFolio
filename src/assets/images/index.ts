df449cd1c97f635a1d902ef189b2f222817fb191
ashithprakas <ashithprakas@gmail.com>
2024-07-09 21:58:19 +0530

    Production Build

--- src/assets/images/index.ts
+++ src/assets/images/index.ts
@@ -1,11 +1,11 @@
-import LeftArrow from "./arrow1.svg";
-import Logo from "./logo.svg";
-import LinkedInIcon from "./nav-icon1.svg";
-import FaceBookIcon from "./nav-icon2.svg";
-import InstagramIcon from "./nav-icon3.svg";
-import HeaderIcon from "./header-img.svg";
-import GradientBackground1 from "./color-sharp.png";
-import GradientBackground2 from "./color-sharp2.png";
+import LeftArrow from "/src/assets/images/arrow1.svg";
+import Logo from "/src/assets/images/logo.svg";
+import LinkedInIcon from "/src/assets/images/nav-icon1.svg";
+import FaceBookIcon from "/src/assets/images/nav-icon2.svg";
+import InstagramIcon from "/src/assets/images/nav-icon3.svg";
+import HeaderIcon from "/src/assets/images/header-img.svg";
+import GradientBackground1 from "/src/assets/images/color-sharp.png";
+import GradientBackground2 from "/src/assets/images/color-sharp2.png";
 
 export {
   Logo,
